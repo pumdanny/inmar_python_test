89258b19713dadd110f0fab781dbf5cbc903ca96
pumadanny <ingdanieldelangel@gmail.com>
2025-07-28 13:05:28 -0600

    Adding module 2

--- DATA_PROCESSOR.py
+++ DATA_PROCESSOR.py
@@ -2,7 +2,7 @@ from ast import Module
 import re
 import os
 from datetime import date
-
+import pandas as pd
 
 class DATA_PROCESSOR:
     def __init__(self ):
@@ -13,9 +13,12 @@ class DATA_PROCESSOR:
 
         self.input_path = f"{current_dir}\data\input"
         self.processing_path =f"{current_dir}\data\processing"
-        self.output_path = f"{current_dir}\data\output"
+        self.error_path = f"{current_dir}\data\error"
+        self.success_path = f"{current_dir}\data\success"
 
         self.list_of_files = []
+
+        self.files_to_be_processed = []
 
     def get_files_to_process(self):
         # find the sources files that match the pattern
@@ -43,8 +46,6 @@ class DATA_PROCESSOR:
                 file_to_move = os.path.join(self.input_path, filename)
                 os.rename(file_to_move, os.path.join(self.error_path, filename))
 
-
-
     def load_data_module_1(self):
         self.get_files_to_process()
 
@@ -61,6 +62,8 @@ class DATA_PROCESSOR:
 
                     file_to_process = os.path.join(self.processing_path, file)
                     print(f"Processing file: {file_to_process}")
+                    self.files_to_be_processed.append(file_to_process)
+
                 else:
                     print(f"File {file} is empty. Moving to error folder.")
                     # Move the file to the error folder
@@ -74,18 +77,102 @@ class DATA_PROCESSOR:
         # check if file is not empty
         return os.path.getsize(file_name) == 0
             
-    def process_data(self):
-        # Process the loaded data
-        pass
+    def data_quality_check_module_2(self ):
+        
+        for file_name in self.files_to_be_processed:
+            # read the file into a pandas dataframe with header row
+            print(f"Loading data from {file_name}...")
+            df = pd.read_csv(file_name, header=0, encoding='utf-8')
+            file_name_only = os.path.basename(file_name)
+            print(f"Data loaded from {file_name_only}. Number of rows: {len(df)}")
 
-    def save_data(self, destination):
-        # Save the processed data to the specified destination
-        pass
+            # print the first 5 rows of the dataframe
+            print("First 5 rows of the dataframe:")
+            print(df['phone'].head())
+
+
+            # cast phone to string all the phone numbers
+            df['phone'] = df['phone'].astype(str)
+            
+            # spliting into two columns contact_number_1 and contact_number_2
+            # splited by \n
+            df[['contact_number_1', 'contact_number_2']] = df['phone'].str.split('\n', n=1, expand=True)
+            
+
+            # remove "+" and spaces , \r and \n from the phone number
+            df['contact_number_1'] = df['contact_number_1'].str.replace(r"[+ ]", "", regex=True)
+            df['contact_number_1'] = df['contact_number_1'].str.replace(r"[\r\n]", "", regex=True)
+            df['contact_number_2'] = df['contact_number_2'].str.replace(r"[+ ]", "", regex=True)
+            df['contact_number_2'] = df['contact_number_2'].str.replace(r"[\r\n]", "", regex=True)
+
+            # validate phone numbers
+            df['phone_valid_1'] = df['contact_number_1'].apply(self.check_phone_file)
+            df['phone_valid_2'] = df['contact_number_2'].apply(self.check_phone_file)
+
+            #filter out invalid phone numbers
+            valid_df_0 = df[(df['phone_valid_1'] == True) & (df['phone_valid_2'] == True)]
+            print(f"Number of valid phone numbers: {len(valid_df_0)}")
+
+            #filter out invalid phone numbers
+            valid_df_1 = df[(df['phone_valid_1'] == True) & (df['phone_valid_2'] == False)]
+            print(f"Number of valid phone numbers: {len(valid_df_1)}")
+            # filter out invalid phone numbers for second phone number
+
+            #filter out invalid phone numbers
+            valid_df_2 = df[(df['phone_valid_1'] == False) & (df['phone_valid_2'] == True)]
+            print(f"Number of valid phone numbers: {len(valid_df_1)}")
+            # filter out invalid phone numbers for second phone number
+
+            # join valid_df_0, valid_df_1 and valid_df_2
+            valid_df = pd.concat([valid_df_0, valid_df_1, valid_df_2], ignore_index=True)
+
+            # filter out invalid phone numbers for first phone number
+            invalid_df_phone = df[(df['phone_valid_1'] == False) & (df['phone_valid_2'] == False)]
+            print(f"Number of invalid phone numbers: {len(invalid_df_phone)}")
+
+            # if phone number one or phone number two (or both) is ok I will keep the record
+            # save invalid data to error folder 
+            output_file = os.path.join(self.error_path, f"{file_name_only[0:-4]}_invalid_data_phone_number_{date.today()}.bad")
+            invalid_df_phone.to_csv(output_file, index=False)
+            print(f"Invalid data saved to {output_file}")
+
+            # validate that the required fields are not null
+            required_fields = ['name', 'contact_number_1', 'location']
+            non_null_df = valid_df.dropna(subset=required_fields)
+            print(f"Registros con todos los campos requeridos no nulos: {len(non_null_df)}")
+            # print registros con todos los campos requeridos no nulos
+            print(non_null_df.head())
+
+            # get records where at least one of the required fields is null or empty or ""
+            null_df = valid_df[valid_df[required_fields].isnull().any(axis=1) | (valid_df[required_fields] == "").any(axis=1)]
+            print(f"Registros con al menos un campo requerido nulo: {len(null_df)}")
+            # print registros con al menos un campo requerido nulo
+            print(null_df.head())
+
+            # save records with at least one required field null to error folder
+            null_output_file = os.path.join(self.error_path, f"{file_name_only[0:-4]}_null_data_{date.today()}.bad")
+            null_df.to_csv(null_output_file, index=False)
+            print(f"Records with at least one required field null saved to {null_output_file}")
+            
+
+    def check_phone_file(self, phone: None):      
+        response = False
+        # Check if the file contains phone numbers in the correct format
+        regex = r"^\d{10,15}$"
+
+        if phone is None or phone == "":
+            return False
+        else:
+            response = re.match(regex, phone) is not None
+            if not response:
+                print(f"Invalid phone number format: {phone}. Expected format: 10 to 15 digits.")
+            return response
+
 
 if __name__ == "__main__":
     processor = DATA_PROCESSOR()
     processor.load_data_module_1()
-    #processor.process_data()
+    processor.data_quality_check_module_2()
     #processor.save_data("processed_data.txt")
 
 
